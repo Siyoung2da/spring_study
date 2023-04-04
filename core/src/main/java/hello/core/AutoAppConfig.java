afee0339e77ef0466271a88e11b2d6f9b7c0591d
Siyoung Park <drq5@daum.net>
2023-04-05 01:34:01 +0900

    중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,9 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,10 +12,13 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         basePackages = "hello.core.member",
         // 예제 유지할려고
-        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 
 public class AutoAppConfig {
 
-
+    @Bean(name = "memoryMemberRepository")  // 자동 빈 겹칠 때는 오류! 수동 빈 vs 자동 빈 만나면 수동 빈이 오버라이딩함.
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
