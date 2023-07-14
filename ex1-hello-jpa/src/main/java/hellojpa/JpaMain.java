99fde303f7c103c238a6fb457b5b7a8047823f6a
Siyoung Park <drq5@daum.net>
2023-07-15 00:00:14 +0900

    영속성 컨텍스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,16 +18,12 @@ public class JpaMain {
 
         try {
 
-//            Member findMember = em.find(Member.class, 1L);
+            //영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            System.out.println(" ======= ");
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
