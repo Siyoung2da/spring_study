b5c52bf4fe197360632f61073ecbd79705632f33
Siyoung Park <drq5@daum.net>
2023-07-21 15:36:57 +0900

    일대일 [1:1]

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -18,7 +18,11 @@ public class Member {
 
     @ManyToOne
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
-    private Team team
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
