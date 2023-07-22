2179ddd5ee84d41f9b7bf40ed8dcbd013fb78cb1
Siyoung Park <drq5@daum.net>
2023-07-22 10:56:11 +0900

    실전 예제 4-상속관계 매핑

--- ex1-hello-jpa/src/main/java/jpabook/jpashop/domain/Movie.java
+++ ex1-hello-jpa/src/main/java/jpabook/jpashop/domain/Movie.java
@@ -1,9 +1,10 @@
-package hellojpa;
+package jpabook.jpashop.domain;
 
 import javax.persistence.Entity;
 
-//@Entity
+@Entity
 public class Movie extends Item {
+
     private String director;
     private String actor;
 
