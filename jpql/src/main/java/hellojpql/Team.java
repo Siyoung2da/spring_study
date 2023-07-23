fcddeab33c37e88597140105e36137e1b0ba5375
Siyoung Park <drq5@daum.net>
2023-07-23 12:22:01 +0900

    조인

--- jpql/src/main/java/hellojpql/Team.java
+++ jpql/src/main/java/hellojpql/Team.java
@@ -19,6 +19,7 @@ public class Team {
     private List<Member> members = new ArrayList<>();
 
 
+
     public Long getId() {
         return id;
     }
@@ -33,5 +34,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
